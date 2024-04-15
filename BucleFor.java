97e8218a933981b52b040ddaa6f0f8584e38e2a2
Elis Lopez <elismiguellopezgonzalez@hotmail.com>
2024-04-15 12:43:40 -0400

    reorganizacion del repositorio

--- BucleFor.java
+++
@@ -1,7 +0,0 @@
-public class app{
-  public static void main(String[]args){
-    for(int i=1;i<=10;i++){
-      System.out.println("Estoy en la vuelta " + i);
-    }
-  }
-}
--- BucleWhile.java
+++
@@ -1,9 +0,0 @@
-public class app{
-  public static void main(String[]args){
-    int vuelta=1;
-    while(vuelta<=20){
-      System.out.println("Estoy en la vuelta " + vuelta);
-      vuelta=vuelta+1;
-    }
-  }
-}
